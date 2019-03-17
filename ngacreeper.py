2b25ab0a936372f8734a8ad54d89ba3a20b9552e
voidf <510030805@qq.com>
2019-03-17 21:20:05 +0800

    add sl function

--- ngacreeper.py
+++ ngacreeper.py
@@ -1,7 +1,5 @@
 import requests,re,os,sys,json,copy
 from bs4 import BeautifulSoup
-# from requests.adapters import HTTPAdapter
-# from requests.packages.urllib3.util.ssl_ import create_urllib3_context
 
 users_id=[]#投票用户id
 comment_raw=[]#楼层投票原生字符串
@@ -10,143 +8,161 @@ kansens=[]#参赛舰舰名
 votes=[]#参赛舰获票数
 floor=0
 try:
-	ignore_dict=open("ngaIgnore_dict.txt","r")
-	ignore_dict.close()
+	with open("Result.json","r") as f:
+		firstline=json.loads(f.readline())
+		if "kansens" in firstline and "votes" in firstline:
+			if len(firstline["kansens"])==len(firstline["votes"]):
+				print("发现上次未完成的记录，是否重新载入？")
+				askconf=input("输入y以确认，否则请按回车：")
+				if askconf!="y":
+					raise NameError("Reload Denied")
+				kansens=firstline["kansens"]
+				votes=firstline["votes"]
+				comment_process=[]
+				for i in f:
+					js=json.loads(i)
+					comment_ind.append(js["floor"])
+					users_id.append(js["user"])
+					comment_process.append(js["comment"])
 except:
-	print("警告：尚未建立忽略字典，新建文件ngaIgnore_dict.txt于本目录")
-	ignore_dict_temp=open("ngaIgnore_dict.txt","w")
-	ignore_dict_temp.close()
-
-try:
-	translate_dict=open("ngaTranslate_dict.txt","r")
-	translate_dict.close()
-except:
-	print("警告：尚未建立舰名转换字典，新建文件ngaTranslate_dict.txt于本目录")
-	translate_dict_temp=open("ngaTranslate_dict.txt","w")
-	translate_dict_temp.close()
-
-lnk=input("请输入投票页链接，留空则用默认调试页")
-if lnk=='':
-	lnk="https://bbs.nga.cn/read.php?tid=16626917"
-ses=requests.session()
-hds={
-	"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-	"Accept-Encoding":"gzip, deflate, br",
-	"Accept-Language":"zh-CN,zh;q=0.9",
-	"Cache-Control":"max-age=0",
-	"Connection":"keep-alive",
-	"Host":"bbs.nga.cn",
-	"Referer":"https://bbs.nga.cn/read.php?tid=16626917&_ff=564",
-	"Upgrade-Insecure-Requests":"1",
-	"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-}
-hds["Cookie"]=input("(调试时可选)Cookie:")
-if hds["Cookie"]=='':
 	try:
-		with open("ngack.txt","r") as fr:
-			hds["Cookie"]=fr.read()
+		ignore_dict=open("ngaIgnore_dict.txt","r")
+		ignore_dict.close()
 	except:
-		hds["Cookie"]="taihe=993152ac186e4e29eb6da7a79cec80a7; taihe_session=7b139c8bcd002cd6282c34283639775e; UM_distinctid=16984dafa44593-0bba1cfe5cb1d9-43450521-1fa400-16984dafa45bb2; CNZZDATA30043604=cnzz_eid%3D70536919-1552710109-%26ntime%3D1552710109; CNZZDATA30039253=cnzz_eid%3D1371588293-1552710731-%26ntime%3D1552710731; Hm_lvt_5adc78329e14807f050ce131992ae69b=1552712137; ngaPassportOid=b9d996ccc7365574b2feef4c7a26f56c; ngacn0comUserInfo=CreeperUsers_voidf%09CreeperUsers_voidf%0939%0939%09%0910%090%090%090%090%09; ngacn0comUserInfoCheck=b728822895080f88d013d477be025f86; ngacn0comInfoCheckTime=1552712246; ngaPassportUid=60301802; ngaPassportUrlencodedUname=CreeperUsers_voidf; ngaPassportCid=X8opugmvmo688obredmpk49c7bkdbu8v97u78r2k; CNZZDATA1262314542=1985232761-1552708712-https%253A%252F%252Fbbs.nga.cn%252F%7C1552708712; lastvisit=1552712262; lastpath=/read.php?tid=16626917&_ff=564; bbsmisccookies=%7B%22uisetting%22%3A%7B0%3A%22e%22%2C1%3A1552712561%7D%2C%22insad_refreshid%22%3A%7B0%3A%22/9Iwlg9B2Wc975ynDjSzUTNs6BXXqTdX7Nsrxhase%22%2C1%3A1553316936%7D%2C%22pv_count_for_insad%22%3A%7B0%3A-45%2C1%3A1552755672%7D%2C%22insad_views%22%3A%7B0%3A1%2C1%3A1552755672%7D%7D; Hm_lpvt_5adc78329e14807f050ce131992ae69b=1552712262"
+		print("警告：尚未建立忽略字典，新建文件ngaIgnore_dict.txt于本目录")
+		ignore_dict_temp=open("ngaIgnore_dict.txt","w")
+		ignore_dict_temp.close()
+
+	try:
+		translate_dict=open("ngaTranslate_dict.txt","r")
+		translate_dict.close()
+	except:
+		print("警告：尚未建立舰名转换字典，新建文件ngaTranslate_dict.txt于本目录")
+		translate_dict_temp=open("ngaTranslate_dict.txt","w")
+		translate_dict_temp.close()
+
+	lnk=input("请输入投票页链接，留空则用默认调试页")
+	if lnk=='':
+		lnk="https://bbs.nga.cn/read.php?tid=16626917"
+	ses=requests.session()
+	hds={
+		"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+		"Accept-Encoding":"gzip, deflate, br",
+		"Accept-Language":"zh-CN,zh;q=0.9",
+		"Cache-Control":"max-age=0",
+		"Connection":"keep-alive",
+		"Host":"bbs.nga.cn",
+		"Referer":"https://bbs.nga.cn/read.php?tid=16626917&_ff=564",
+		"Upgrade-Insecure-Requests":"1",
+		"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+	}
+	hds["Cookie"]=input("(调试时可选)Cookie:")
+	if hds["Cookie"]=='':
+		try:
+			with open("ngack.txt","r") as fr:
+				hds["Cookie"]=fr.read()
+		except:
+			hds["Cookie"]="taihe=993152ac186e4e29eb6da7a79cec80a7; taihe_session=7b139c8bcd002cd6282c34283639775e; UM_distinctid=16984dafa44593-0bba1cfe5cb1d9-43450521-1fa400-16984dafa45bb2; CNZZDATA30043604=cnzz_eid%3D70536919-1552710109-%26ntime%3D1552710109; CNZZDATA30039253=cnzz_eid%3D1371588293-1552710731-%26ntime%3D1552710731; Hm_lvt_5adc78329e14807f050ce131992ae69b=1552712137; ngaPassportOid=b9d996ccc7365574b2feef4c7a26f56c; ngacn0comUserInfo=CreeperUsers_voidf%09CreeperUsers_voidf%0939%0939%09%0910%090%090%090%090%09; ngacn0comUserInfoCheck=b728822895080f88d013d477be025f86; ngacn0comInfoCheckTime=1552712246; ngaPassportUid=60301802; ngaPassportUrlencodedUname=CreeperUsers_voidf; ngaPassportCid=X8opugmvmo688obredmpk49c7bkdbu8v97u78r2k; CNZZDATA1262314542=1985232761-1552708712-https%253A%252F%252Fbbs.nga.cn%252F%7C1552708712; lastvisit=1552712262; lastpath=/read.php?tid=16626917&_ff=564; bbsmisccookies=%7B%22uisetting%22%3A%7B0%3A%22e%22%2C1%3A1552712561%7D%2C%22insad_refreshid%22%3A%7B0%3A%22/9Iwlg9B2Wc975ynDjSzUTNs6BXXqTdX7Nsrxhase%22%2C1%3A1553316936%7D%2C%22pv_count_for_insad%22%3A%7B0%3A-45%2C1%3A1552755672%7D%2C%22insad_views%22%3A%7B0%3A1%2C1%3A1552755672%7D%7D; Hm_lpvt_5adc78329e14807f050ce131992ae69b=1552712262"
+			with open("ngack.txt","w") as fw:
+				fw.write(hds["Cookie"])
+	else:
 		with open("ngack.txt","w") as fw:
 			fw.write(hds["Cookie"])
-else:
-	with open("ngack.txt","w") as fw:
-		fw.write(hds["Cookie"])
 
-#try:
-req=ses.get(lnk,headers=hds)
-#print(req.content.decode('gbk'))
-def catch_page(req,if_first):
-	global users_id
-	global comment_raw
-	global floor
-	sp=BeautifulSoup(req.content,"html.parser")
-	reply_list=sp.find_all("table",attrs={"class":"forumbox postbox"})
-	if if_first==True:
-		host=reply_list.pop(0)#防止楼主进记录,生成参赛者名单
+	#try:
+	req=ses.get(lnk,headers=hds)
+	#print(req.content.decode('gbk'))
+	def catch_page(req,if_first):
+		global users_id
+		global comment_raw
+		global floor
+		sp=BeautifulSoup(req.content,"html.parser")
+		reply_list=sp.find_all("table",attrs={"class":"forumbox postbox"})
+		if if_first==True:
+			host=reply_list.pop(0)#防止楼主进记录,生成参赛者名单
 
-		participants=re.findall(re.compile("jpg\\[/img\\]<br/><br/>(.*?)<br/><br/>\\[/quote\\].*?"),str(host))[0]
+			participants=re.findall(re.compile("jpg\\[/img\\]<br/><br/>(.*?)<br/><br/>\\[/quote\\].*?"),str(host))[0]
 
-		global kansens
-		kansens=participants.split("<br/>")
-		global votes
-		votes=[0 for ii in range(len(kansens))]
-		print(kansens)
-		print(votes)
-		os.system("pause")
-	#retrytime=20
-	for i in reply_list:
-		#print(str(i))
-		flr=int(re.findall(re.compile('''postcontainer(.*?)["'].*?'''),str(i))[0])#'
-		
-		if flr<=floor:
-			# if retrytime>0:
-			#     retrytime-=1
-			#     continue
-			print("检测到楼层重复:%d,%d"%(flr,floor))
-			raise NameError("Capture Completed")
+			global kansens
+			kansens=participants.split("<br/>")
+			global votes
+			votes=[0 for ii in range(len(kansens))]
+			print(kansens)
+			print(votes)
+			os.system("pause")
 		#retrytime=20
-		floor=flr
-		uid=re.findall(re.compile('''uid=(.*?)['"] id=.*?'''),str(i))
-		com=re.findall(re.compile('''<span class=['"]postcontent ubbcode['"] id=['"]postcontent.+['"]>(.*?)</span>.*?'''),str(i))#'
-		users_id.append(uid[0])
-		comment_raw.append(com[0])
-	print(len(users_id))
-	print(len(comment_raw))
-	#except:
-		#pass
+		for i in reply_list:
+			#print(str(i))
+			flr=int(re.findall(re.compile('''postcontainer(.*?)["'].*?'''),str(i))[0])#'
+			
+			if flr<=floor:
+				# if retrytime>0:
+				#     retrytime-=1
+				#     continue
+				print("检测到楼层重复:%d,%d"%(flr,floor))
+				raise NameError("Capture Completed")
+			#retrytime=20
+			floor=flr
+			uid=re.findall(re.compile('''uid=(.*?)['"] id=.*?'''),str(i))
+			com=re.findall(re.compile('''<span class=['"]postcontent ubbcode['"] id=['"]postcontent.+['"]>(.*?)</span>.*?'''),str(i))#'
+			users_id.append(uid[0])
+			comment_raw.append(com[0])
+		print(len(users_id))
+		print(len(comment_raw))
+		#except:
+			#pass
 
-catch_page(req,True)
-page_integer=2
-while True:
-	
-	clnk=lnk+"&page=%d"%page_integer
+	catch_page(req,True)
+	page_integer=2
+	while True:
+		
+		clnk=lnk+"&page=%d"%page_integer
 
-	try:
-		catch_page(ses.get(clnk,headers=hds),False)
-	except:
-		#print(ses.get(clnk,headers=hds))
-		break
-	page_integer+=1
+		try:
+			catch_page(ses.get(clnk,headers=hds),False)
+		except:
+			#print(ses.get(clnk,headers=hds))
+			break
+		page_integer+=1
 
-comment_ind=[int(i) for i in range(len(comment_raw))]
+	comment_ind=[int(i) for i in range(len(comment_raw))]
 
 
-voted_uid=[]
-print("投票有效性验证中……")
+	voted_uid=[]
+	print("投票有效性验证中……")
 
-ifchk=input("是否检测投票用户注册时间？是请输入y：")
-if ifchk=="y":
-	p=1
-	for i in users_id:#根据规则进行一些处理
-		check_reg=ses.get("https://bbs.nga.cn/nuke.php?__lib=ucp&__act=get&lite=js&uid=%s"%i,headers=hds).content.decode("gbk")
-		regdate=int(re.findall(re.compile('''"regdate":(.*?),.*?'''),check_reg)[0])
-		print(p/len(users_id)*100,"%",sep='')
-		p+=1
-		if regdate>1551369600:
-			print("发现用户%s注册时间晚于2019年3月1日零点，抹去该用户投票"%i)
-			while i in users_id:
-				pos=users_id.index(i)
-				del users_id[pos]
-				del comment_raw[pos]
-				del comment_ind[pos]
-			continue
-		elif i not in voted_uid:
-			voted_uid.append(i)
-		else:
-			print("发现用户%s发表了多个回复，只保留第一个"%i)
-			while users_id.count(i)>1:
-				pos=users_id.index(i,users_id.index(i)+1)
-				del users_id[pos]
-				del comment_raw[pos]
-				del comment_ind[pos]
+	ifchk=input("是否检测投票用户注册时间？是请输入y：")
+	if ifchk=="y":
+		p=1
+		for i in users_id:#根据规则进行一些处理
+			check_reg=ses.get("https://bbs.nga.cn/nuke.php?__lib=ucp&__act=get&lite=js&uid=%s"%i,headers=hds).content.decode("gbk")
+			regdate=int(re.findall(re.compile('''"regdate":(.*?),.*?'''),check_reg)[0])
+			print(p/len(users_id)*100,"%",sep='')
+			p+=1
+			if regdate>1551369600:
+				print("发现用户%s注册时间晚于2019年3月1日零点，抹去该用户投票"%i)
+				while i in users_id:
+					pos=users_id.index(i)
+					del users_id[pos]
+					del comment_raw[pos]
+					del comment_ind[pos]
+				continue
+			elif i not in voted_uid:
+				voted_uid.append(i)
+			else:
+				print("发现用户%s发表了多个回复，只保留第一个"%i)
+				while users_id.count(i)>1:
+					pos=users_id.index(i,users_id.index(i)+1)
+					del users_id[pos]
+					del comment_raw[pos]
+					del comment_ind[pos]
 
-#try:
-with open("ngaResult.json","w") as fr:#保存用户投票记录到文件
-	for j in range(len(users_id)):
-		fr.writelines(json.dumps({users_id[j]:comment_raw[j]})+"\n")
-print("投票页面信息获取已经完成，切换到本地数据操作模式，输入h可查看帮助")
-comment_process=copy.deepcopy(comment_raw)
+	#try:
+	with open("ngaResult.json","w") as fr:#保存用户投票记录到文件
+		for j in range(len(users_id)):
+			fr.writelines(json.dumps({users_id[j]:comment_raw[j]})+"\n")
+	print("投票页面信息获取已经完成，切换到本地数据操作模式，输入h可查看帮助")
+	comment_process=copy.deepcopy(comment_raw)
 
 log=open("ngaVoteLog.txt","w")
 
@@ -163,8 +179,27 @@ while True:
 	if cmd=="h":
 		print("可用命令：addig addtr ld auto show ato score manu pass save")
 		print("输入命令以查看具体用法")
-
-
+	elif cmd[:4]=="save":
+		print("此操作将会把剩余未处理的记录保存为文件，输入q以离开")
+		askconf=input("按下空格以继续：")
+		if askconf=="q":
+			continue
+		else:
+			with open("给人看的结果.txt","w") as fwh:
+				fwh.write("舰娘当前计分：\n")
+				for i in range(len(kansens)):
+					fwh.write(kansens[i]+":"+str(votes[i])+"\n")
+				fwh.write("============================================\n")
+				fwh.write("楼层\t用户id\t评论内容\n")
+				with open("Result.json","w") as fw:
+					fw.write(json.dumps({"kansens":kansens,"votes":votes})+"\n")
+					for i in range(len(comment_process)):
+						fw.write(json.dumps({"floor":comment_ind[i],"user":users_id[i],"comment":comment_process[i]})+"\n")
+						fwh.write(str(comment_ind[i])+"\t"+str(users_id[i])+"\t"+comment_process[i]+"\n")
+			print("记录已经保存到当前目录")
+			print("Result.json可用于重载进度")
+			print("重载方法为重新启动本脚本")
+			print("并确保当前工作目录下存在Result.json")
 	elif cmd[:5]=="addig":
 		try:
 			cmdli=cmd[6:].split(" ")
@@ -270,17 +305,21 @@ while True:
 			for jj in range(len(kansens)):
 				print(jj,kansens[jj],cur_vote[jj])
 		
-			askconf=input("输入n以离开,或输入舰娘名或对应下标以修改此票，否则回车将此票计入:")
-			if askconf=="n":
+			askconf=input("输入q以离开,或输入舰娘名或对应下标以修改此票，否则回车将此票计入:")
+			if askconf=="q":
 				break
-			elif kansens.find(askconf)!=-1:
-				tag=kansens.find(askconf)
+			try:
+				tag=kansens.index(askconf)
 				cur_vote[tag]=cur_vote[tag]^1
-			elif askconf.isdigit():
-				cur_vote[askconf]=cur_vote[askconf]^1
+			except:
+				pass
+			if askconf.isdigit():
+				cur_vote[int(askconf)]=cur_vote[int(askconf)]^1
 			elif askconf=='':
 				votes=[votes[ii]+cur_vote[ii] for ii in range(len(votes))]
 				comment_process.pop(0)
+				users_id.pop(0)
+				comment_ind.pop(0)
 				break
 			else:
 				print("无法理解的指令")
@@ -338,13 +377,18 @@ while True:
 				if cur_vote.count(1)>3:
 					print("投票对象大于3个，%s作废"%tpi)
 					comment_process.pop(inte)
+					comment_ind.pop(inte)
+					users_id.pop(inte)
 					continue
 				elif i=='':
-					print("%s的处理结果："%tpi)
-					for jj in range(len(kansens)):
-						print(kansens[jj],cur_vote[jj])
+					# print("%s的处理结果："%tpi)
+					# for jj in range(len(kansens)):
+					# 	print(kansens[jj],cur_vote[jj])
+					print("已处理%s"%tpi)
 					votes=[votes[ii]+cur_vote[ii] for ii in range(len(votes))]
 					comment_process.pop(inte)
+					comment_ind.pop(inte)
+					users_id.pop(inte)
 				else:
 					inte+=1
 	elif cmd[:3]=="ato":
